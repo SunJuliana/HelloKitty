a11db13e5ce32e33c49894fba8bb29a640de79eb
SunJuliana <juliakulisheva@yandex.ru>
2022-06-14 00:40:25 +0700

    Application completed

--- app/src/main/java/com/example/hellokitty/MainActivity.kt
+++ app/src/main/java/com/example/hellokitty/MainActivity.kt
@@ -1,32 +1,36 @@
 package com.example.hellokitty
-
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mHelloTextView: TextView
+    private lateinit var helloTextView: TextView
+    private lateinit var editText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mHelloTextView = findViewById(R.id.textView)
+        helloTextView = findViewById(R.id.textView)
+        editText = findViewById(R.id.editText)
 
         var imageButton: ImageButton = findViewById(R.id.imageButton)
         // альтернативный вариант
         // val imageButton = findViewById<ImageButton>(R.id.imageButton)
         // или val imageButton = findViewById(R.id.imageButton) as ImageButton
 
-       // imageButton.setOnClickListener {
-       //     mHelloTextView.setText("Hello, Kitty!")
+//        imageButton.setOnClickListener {
+//            helloTextView.setText("Hello Kitty")
+//        }
+
         imageButton.setOnClickListener {
             if (editText.text.isEmpty()) {
-                textView.text = "Hello, Kitty!";
-            }else{
-                textView.text = "Привет, "+ editText.text
+                helloTextView.text = "Hello Kitty!";
+            } else {
+                helloTextView.text = "Привет, " + editText.text + "!"
             }
         }
     }
